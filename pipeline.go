a83a1444591375be846489079a6593efa4a29980
yayanyang <yayanyang@gmail.com>
2015-09-10 15:45:16 +0800

    remove defer

--- pipeline.go
+++ pipeline.go
@@ -241,17 +241,17 @@ func (pipeline *_Pipeline) Close() {
 
 func (pipeline *_Pipeline) protectCall(handler *_Handler, f func(handler *_Handler) error) (err error) {
 
-	defer func() {
-		if e := recover(); e != nil {
-
-			if _, ok := e.(error); ok {
-				err = gserrors.Newf(e.(error), "catched pipline exception")
-			} else {
-				err = gserrors.Newf(ErrUnknown, "catched pipline exception :%s", e)
-			}
-		}
-
-	}()
+	// defer func() {
+	// 	if e := recover(); e != nil {
+	//
+	// 		if _, ok := e.(error); ok {
+	// 			err = gserrors.Newf(e.(error), "catched pipline exception")
+	// 		} else {
+	// 			err = gserrors.Newf(ErrUnknown, "catched pipline exception :%s", e)
+	// 		}
+	// 	}
+	//
+	// }()
 
 	handler.Lock()
 	defer handler.Unlock()
