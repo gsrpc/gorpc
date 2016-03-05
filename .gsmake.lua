f833e906964140ccc878e42cb387d6e135b027f4
unknown <unknown@unknown.com>
2016-03-05 18:47:35 +0800

    update

--- .gsmake.lua
+++ .gsmake.lua
@@ -10,6 +10,7 @@ properties.golang = {
         { name = "github.com/gsdocker/gserrors" };
         { name = "github.com/gsdocker/gsconfig" };
         { name = "github.com/gsdocker/gslogger" };
+        { name = "github.com/gorilla/websocket" };
     };
 
     tests = { "timer","hashring" };
